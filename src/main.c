db311e730344c351311eeed3d9f23f60da99bd8f
Mike Felida <mifelida@student.codam.nl>
2025-05-26 17:06:04 +0200

    More Norm

--- src/main.c
+++ src/main.c
@@ -6,14 +6,14 @@
 /*   By: mifelida <mifelida@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:25:36 by mifelida          #+#    #+#             */
-/*   Updated: 2025/05/26 15:00:18 by mifelida         ###   ########.fr       */
+/*   Updated: 2025/05/26 17:05:25 by mifelida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int	main(void)
 {
 	printf("Hello, World!\n");
 	return (EXIT_SUCCESS);
