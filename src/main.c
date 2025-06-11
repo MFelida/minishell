1349f96a6ad4539e70f0b720bd47efe41a76c36c
Amazigh El Founti <amel-fou@f1r4s6.codam.nl>
2025-06-11 14:05:29 +0200

    gameplan is there, understanding has grown, LOCK TF IN >:0

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amel-fou <amel-fou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 14:25:36 by mifelida          #+#    #+#             */
-/*   Updated: 2025/06/05 18:13:27 by amel-fou         ###   ########.fr       */
+/*   Updated: 2025/06/11 14:04:19 by amel-fou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,28 @@
 #include <readline/history.h>
 #include <signal.h>
 #include "parsing_header.h"
+#include <unistd.h>
 
-void	signal_handler(int	signal)
+void	signal_handler(int signal)
 {
 	(void)signal;
+	write(1, "\n", 1);
 	rl_on_new_line();
-	rl_replace_line("\n", 1);
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
-int	main(void)
+int	main(void) // https://www.cs.sjsu.edu/faculty/pearce/java1/streams/tokens.html
 {
 	char	*input;
 
+	// tokenize input (think operators and identifiers, whitespace)
+	// check for commands, check if operators are control, otherwise split metachars
+	// set up tree and send it over
+	// tokenize, put in struct (pointer next (FIRST), token, string), then organize for right-
+	// order in tree
+
+	rl_catch_signals = 0;
 	signal(SIGINT, signal_handler);
 	while (1)
 	{
@@ -36,12 +45,12 @@ int	main(void)
 		if (!input)
 		{
 			printf("exit\n");
-			break;
+			break ;
 		}
 		if (*input)
 		{
 			add_history(input);
-			//parser_sender(input);
+			// parser_processor(input);
 		}
 		printf("your command: %s\n", input);
 		free(input);
@@ -50,42 +59,77 @@ int	main(void)
 	return (EXIT_SUCCESS);
 }
 
-int	parser_processer(char *input)
+int	parser_processor(char *input)
 {
-	int		i;
-	int		apo_bool;
-	char	*seperated_input;
+	int	i;
+	int	j;
+	char *new_input;
 
-	apo_bool = 0;
+	j = 0;
 	i = 0;
-	while(input[i] != '\0')
+	while(isspace(input[i]))
+	i++;
+	if (input[i] == '"')
 	{
-		while (ft_isspace(input[i]) && apo_bool != 1)
-			i++;
-		while (!ft_isspace(input[i]) || apo_bool == 1)
+		while(input[++i] != '"')
 		{
-			if (input[i] == '"')
-			{
-				if (apo_bool == 1)
-					apo_bool = 0;
-				else
-					apo_bool = 1;
-				i++;
-			}
-			// basically strcpy but with comma ',' seperators between each token, then pass it to split with , as the c
-			// then you will have a malloced 2d array with each token and can look at them independently
-			// might be the move
-			// think ft_split for argument intake as well
+			new_input[j] = input[i];	
 		}
 	}
 }
 
-int	within_apo(char *input, int i)
+int	pipe_finder(char *input)
 {
-	while(input[i] != '"')
+	int	i;
+	int	pipecount;
+
+	i = 0;
+	pipecount = 0;
+	while(input[i])
+	{
+		if (input[i] == "|")
+			pipecount++;
+		i++;
+	}
+	return (pipecount);
 }
 
-int	parser_sender(char *input)
-{
+// int	parser_processer(char *input)
+// {
+// 	int		i;
+// 	int		apo_bool;
+// 	char	*seperated_input;
+
+// 	apo_bool = 0;
+// 	i = 0;
+// 	while(input[i] != '\0')
+// 	{
+// 		while (ft_isspace(input[i]) && apo_bool != 1)
+// 			i++;
+// 		while (!ft_isspace(input[i]) || apo_bool == 1)
+// 		{
+// 			if (input[i] == '"')
+// 			{
+// 				if (apo_bool == 1)
+// 					apo_bool = 0;
+// 				else
+// 					apo_bool = 1;
+// 				i++;
+// 			}
+// 			// basically strcpy but with comma ',' seperators between each token, then pass it to split with , as the c
+// 			// then you will have a malloced 2d array with each token and can look at them independently
+// 			// might be the move
+// 			// think ft_split for argument intake as well
+// 		}
+// 	}
+// }
+
+// int	within_apo(char *input, int i)
+// {
+// 	while(input[i] != '"')
+// }
+
+// int	parser_sender(char *input)
+// {
 	
-}
+// }
