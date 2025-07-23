80ee031ffc441e9c6de578b395f57e5d57e41376
Amazigh El Founti <amel-fou@f1r2s8.codam.nl>
2025-07-23 15:26:43 +0200

    trynna work from the ground up so I have oversight. Good progress idc

--- src/utils.c
+++ src/utils.c
@@ -6,15 +6,62 @@
 /*   By: amel-fou <amel-fou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 14:04:58 by amel-fou          #+#    #+#             */
-/*   Updated: 2025/06/03 14:06:46 by amel-fou         ###   ########.fr       */
+/*   Updated: 2025/07/23 14:21:19 by amel-fou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+#include "parsing_header.h"
+#include "tokens.h"
 int	ft_isspace(char c)
 {
 	if (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t')
 		return (1);
 	return (0);
+}
+
+int	ft_isredirapp(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '>' && str[i + 1] == '>')
+		return (MS_FILE_APPEND);
+	else
+		return (-1);
+}
+
+int	ft_isheredoc(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '<' && str[i + 1] == '<')
+		return (MS_HEREDOC);
+	else
+		return (-1);
+}
+
+int	ft_isoutput(char *str)
+{
+	if (str[0] = '>')
+		return (MS_FILE_OUTPUT);
+	else
+		return (-1);
+}
+
+int	ft_isoutput(char *str)
+{
+	if (str[0] = '<')
+		return (MS_FILE_INPUT);
+	else
+		return (-1);
+}
+
+int	ft_ispipe(char *str)
+{
+	if (str[0] = '|')
+		return (MS_PIPE);
+	else
+		return (-1);
 }
