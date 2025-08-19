a8f324284bd1764e32cff00c7ce31b86d71c23b3
Mike Felida <mifelida@f0r4s17.codam.nl>
2025-08-19 13:27:37 +0200

    less leak but still leak

--- src/env/env.c
+++ src/env/env.c
@@ -6,7 +6,7 @@
 //   By: mifelida <mifelida@student.email.com>       +#+                      //
 //                                                  +#+                       //
 //   Created: 2025/08/19 10:42:11 by mifelida     #+#    #+#                  //
-//   Updated: 2025/08/19 12:53:47 by mifelida     ########   odam.nl          //
+//   Updated: 2025/08/19 13:24:50 by mifelida     ########   odam.nl          //
 //                                                                            //
 // ************************************************************************** //
 
@@ -14,6 +14,7 @@
 #include "libft.h"
 
 #include <stddef.h>
+#include <stdlib.h>
 
 static t_hm	env_hm = {0};
 
@@ -36,9 +37,11 @@ int	init_env(void)
 		key = ft_substr(environ[i], 0, value - environ[i] - 1);
 		if (hm_set_value(&env_hm, key, value))
 		{
+			free(key);
 			free_env();
 			return (1);
 		}
+		free(key);
 		i++;
 	}
 	return (0);
