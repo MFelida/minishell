d42cef92cf8200122bbfecab1b87bc9f4af654e9
Mike Felida <mifelida@f0r3s2.codam.nl>
2025-09-30 16:06:21 +0200

    echo can only fail on io error

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -24,21 +24,24 @@ int	ms_echo(char **args, t_cmd_params *params, ...)
 	size_t	i;
 	size_t	first;
 	bool	nl;
+	int		write_error;
 
 	i = 0;
 	nl = true;
+	write_error = MS_SUCCESS;
 	while (args[++i] && _is_option(args[i]) && !_invalid_option(args[i], "n"))
 			nl = !ft_strchr(args[i], 'n');
 	first = i;
 	while (args[i])
 	{
-		if (i > first)
-			ft_putchar_fd(' ', STDOUT_FILENO);
-		ft_putstr_fd(args[i], 1);
+		if (i > first && ft_putchar_fd(' ', STDOUT_FILENO) < 0)
+			write_error = MS_FAILURE;
+		if (ft_putstr_fd(args[i], 1))
+			write_error = MS_FAILURE;
 		i++;
 	}
-	if (nl)
-		ft_putchar_fd('\n', STDOUT_FILENO);
-	params->wstatus = _set_wstatus(MS_SUCCESS, 0);
+	if (nl && ft_putchar_fd('\n', STDOUT_FILENO) < 0)
+		write_error = MS_FAILURE;
+	params->wstatus = _set_wstatus(write_error, 0);
 	return (MS_CMD_ERROR_OK);
 }
