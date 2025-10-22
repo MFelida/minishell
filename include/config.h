52582c458374ebb6458e6f58d43af035183c33f5
Mike Felida <mike@m3f5.dev>
2025-10-22 15:55:28 +0200

    FAST

---
+++ include/config.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   config.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mifelida <mifelida@student.codam.nl>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 15:49:58 by mifelida          #+#    #+#             */
+/*   Updated: 2025/10/22 15:51:22 by mifelida         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONFIG_H
+# define CONFIG_H
+
+# define MS_SHELL_NAME "minishell"
+# define MS_DEFAULT_PROMPT "minishell$ "
+
+#endif // CONFIG_H
